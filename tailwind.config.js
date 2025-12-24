d41d4d3d663bc43a02990c5e64d2649333c9ea0d
NishDevan <danishputradevananda1@gmail.com>
2025-12-24 14:55:54 +0700

    Fix Tailwind config paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,jsx,ts,tsx,mdx}',
-    './pages/**/*.{js,jsx,ts,tsx,mdx}',
-    './components/**/*.{js,jsx,ts,tsx,mdx}',
+    './src/app/**/*. {js,jsx,ts,tsx,mdx}',
+    './src/pages/**/*.{js,jsx,ts,tsx,mdx}',
+    './src/components/**/*.{js,jsx,ts,tsx,mdx}',
   ],
   theme: {
     extend: {
-      colors: {
+      colors:  {
         dark: '#1a1a1a',
         darker: '#0f0f0f',
       },
